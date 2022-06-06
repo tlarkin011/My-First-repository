06f4407bb1bf94483c1bb01a70afe54dbe7a8ae7
tlarkin011 <texas212222@gmail.com>
2022-06-06 10:06:35 -0700

    changed lab

--- Labs/VariablesAndOperations.js
+++ Labs/VariablesAndOperations.js
@@ -1,39 +1,63 @@
-//create a variable to hold the quantity of available plane seats left on a flight
-let availablePlaneSeats = 6;
-		
-//create a variable to hold the cost of groceries at checkout
-let costOfGroceries = 23.64;
+// create a variable called speedLimit and another called mySpeed
+// set their values to numbers between 1 and 100 
+var speedLimit = 50;
+var mySpeed = 65;
+// using a conditional, determine if mySpeed is greater than the speedLimit
+// if true, print "Slow Down! Mom is mad!" to the console
+// if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+// if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+if (mySpeed > speedLimit){ console.log(" Slow down! mom is mad!")
+}
 
-//create a variable to hold a person's middle initial
-let middleInitial = "J";
-
-//create a variable to hold true if it's hot outside and false if it's cold outside
-let isHotOutside = false;
-//create a variable to hold a customer's first name
-let customerFirstName ='Sally';
-
-//create a variable to hold a street address
-let streetAddress = '1234 W Baller Ln' 
-
-//print all variables to the console
-console.log(availablePlaneSeats);
-console.log(costOfGroceries);
+// create two variables, one named alarmSet, the other openDoor
+// set them to a boolean value
 
 
-//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+// using a conditional, determine if alarm is set. 
+// if alarm is set and door is set to open, print "Sound Alarm!" to the console
+// otherwise, print "Everything is fine." to the console.
 
 
-//impulse candy bar purchase, add 2.15 to the grocery total
-//costOfGroceries = costOfGroceries + 2.15;
 
 
-//birth certificate was printed incorrectly, change the middle initial to something else
+// create two variables, username and password
+// create a conditional, if the username is "Tommy123" and the password is "12345"
+// or the username is "Timmy456" and the password is "6789", print "Admin Login Successful" to the console
+// otherwise, print "Admin Access Denied"
 
 
-//the season has changed, update the hot outside variable to be opposite of what it was
 
 
-//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+// write code that will set the value of studentClass based on studentGrade
+// studentGrade will be K-12
+// K-6 will be Elementary
+// 7-8 will be Middle
+// 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
+// Anything other than these values will return "Error" to the console
 
 
-//print a line to the console that introduces the customer and says they live at the address variable
+
+
+
+// write a for loop that will iterate backwards from 10 to -10
+
+
+
+// write a do/while loop that prints 1 through 50
+
+
+// edit the previous do/while loop so that it prints the remainder when the loop number is divided by 4
+
+
+// Someone messed up the following for loop
+// fix the following infinite loop, uncomment to test
+/* 
+    for(let i = 11; i > 10; i++){
+        console.log(i);
+    } 
+*/
+
+
+
+
+
